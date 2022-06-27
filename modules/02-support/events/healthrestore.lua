a4f78a70085cb2f2e7a2a50b9f8490f2467326ed
miah-sebastian <jeremiah_robertson@live.com>
2022-06-27 18:51:03 -0400

    rework healing module to be more calculated.

--- modules/02-support/events/healthrestore.lua
+++ modules/02-support/events/healthrestore.lua
@@ -3,15 +3,109 @@ SupportModule.AutoHeal = {}
 AutoHeal = SupportModule.AutoHeal
 
 local nextHeal = {}
+local lastCast = 0
+local lastUse = 0
+local lastUseDelay = 0
+
+local spellLastUse = {}
+local spellCooldowns = {}
+local groupCooldowns = {}
+local groupLastUse = {}
 
 local settings = {
   [RestoreType.cast] = 'AutoHeal',
   [RestoreType.item] = 'AutoHealthItem'
 }
 
-function AutoHeal.onHealthChange(player, health, maxHealth, oldHealth, restoreType, tries)
-  local tries = tries or 10
+function AutoHeal.onSpellCooldown(spellId, delay)
+	spellCooldowns[spellId] = delay/1000
+	spellLastUse[spellId] = os.clock()
+	return
+end
 
+function AutoHeal.onSpellGroupCooldown(groupId, delay)
+	groupCooldowns[groupId] = delay/1000
+	groupLastUse[groupId] = os.clock()
+	return
+end
+
+function AutoHeal.onMultiUseCooldown(delay)
+	lastUseDelay = delay/1000
+	lastUse = os.clock()
+	return
+end
+
+function AutoHeal.getNextUse(spellId)
+	local cooldownDelay = spellCooldowns[spellId]
+	local lastSpellUse = spellLastUse[spellId]
+	local delayCheck = cooldownDelay or 0
+	local lastUseCheck = lastSpellUse or 0
+	return delayCheck + lastUseCheck
+end
+
+function AutoHeal.onTalk(name, level, mode, message, channelId, creaturePos)
+    local player = g_game.getLocalPlayer()
+    if not player then return end
+	local Panel = SupportModule.getPanel()
+	if not Panel:getChildById(settings[RestoreType.cast]):isChecked() then
+		return -- has since been unchecked
+	end
+	local spellText = Panel:getChildById('HealSpellText'):getText()
+	local playerName = player:getName()
+	if name == playerName and mode == MessageModes.BarkLow and message == spellText then
+		lastCast = os.clock()
+		if nextHeal[RestoreType.cast] then
+			removeEvent(nextHeal[RestoreType.cast])
+		end
+		local player = g_game.getLocalPlayer()
+		if not player then return end
+		local cooldownDelay = spellCooldowns[spellId]
+		local delay = cooldownDelay or 1000
+
+		nextHeal[RestoreType.cast] = scheduleEvent(AutoHeal.onHealthChange(player,
+		player:getHealth(), player:getMaxHealth(), player:getHealth(),
+		RestoreType.cast), delay)
+	end
+	return
+end
+
+function AutoHeal.execute(player)
+	local currentTime = os.clock()
+	local Panel = SupportModule.getPanel()
+	if not Panel:getChildById(settings[RestoreType.cast]):isChecked() then
+		return -- has since been unchecked
+	end
+	local spellText = Panel:getChildById('HealSpellText'):getText()
+    local healthValue = Panel:getChildById('HealthBar'):getValue()
+	local spell = Spells.getSpellByWords(spellText)
+	local spellCheck = spell ~= nil
+	local spellCooldown = spellCheck and AutoHeal.getNextUse(spell.id) or 0
+    
+	local ping = g_game.getPing()/1000
+	local timeCheck = spellCooldown - ping - currentTime or 0
+	local lastCheck = timeCheck > 0
+    local delay = lastCheck and timeCheck or 0
+    if player:getHealthPercent() < healthValue and delay == 0 then
+      g_game.talk(spellText)
+	BotLogger.debug("".. delay)
+	  if nextHeal[RestoreType.cast] then
+		removeEvent(nextHeal[RestoreType.cast])
+	  end
+    end
+	if nextHeal[RestoreType.cast] then
+		removeEvent(nextHeal[RestoreType.cast])
+	end
+	nextHeal[RestoreType.cast] = scheduleEvent(function()
+		local player = g_game.getLocalPlayer()
+		if not player then return end
+		AutoHeal.execute(player)
+	end, 100)
+	return
+end
+		
+
+function AutoHeal.onHealthChange(player, health, maxHealth, oldHealth, restoreType)
+  local currentTime = os.clock()
   local Panel = SupportModule.getPanel()
   if not Panel:getChildById(settings[restoreType]):isChecked() then
     return -- has since been unchecked
@@ -20,25 +114,24 @@ function AutoHeal.onHealthChange(player, health, maxHealth, oldHealth, restoreTy
   if restoreType == RestoreType.cast then
     local spellText = Panel:getChildById('HealSpellText'):getText()
     local healthValue = Panel:getChildById('HealthBar'):getValue()
+	local spell = Spells.getSpellByWords(spellText)
+	local spellCheck = spell ~= nil
+	local spellCooldown = spellCheck and AutoHeal.getNextUse(spell.id) or 0
     
-    local delay = 0
-    if player:getHealthPercent() < healthValue then
-      g_game.talk(spellText)
-      delay = Helper.getSpellDelay(spellText)
+	local ping = g_game.getPing()/1000
+	local timeCheck = spellCooldown - ping - currentTime or 0
+	local lastCheck = timeCheck > 0
+    local delay = lastCheck and timeCheck or 0
+	--BotLogger.debug("".. delay)
+    if player:getHealthPercent() < healthValue and delay == 0 then
+      --g_game.talk(spellText)
+	  if nextHeal[RestoreType.cast] then
+		removeEvent(nextHeal[RestoreType.cast])
+	  end
     end
 
-    nextHeal[RestoreType.cast] = scheduleEvent(function()
-      local player = g_game.getLocalPlayer()
-      if not player then return end
-
-      health, maxHealth = player:getHealth(), player:getMaxHealth()
-      if player:getHealthPercent() < healthValue and tries > 0 then
-        tries = tries - 1
-        AutoHeal.onHealthChange(player, health, maxHealth, health, restoreType, tries) 
-      else
-        removeEvent(nextHeal[RestoreType.cast])
-      end
-    end, delay)
+	AutoHeal.execute(player)
+	return
 
   elseif restoreType == RestoreType.item then
 
@@ -59,9 +152,8 @@ function AutoHeal.onHealthChange(player, health, maxHealth, oldHealth, restoreTy
       local player = g_game.getLocalPlayer()
       if not player then return end
       health, maxHealth = player:getHealth(), player:getMaxHealth()
-      if player:getHealthPercent() < healthValue and tries > 0 then
-        tries = tries - 1
-        AutoHeal.onHealthChange(player, health, maxHealth, health, restoreType, tries) 
+      if player:getHealthPercent() < healthValue then
+        AutoHeal.onHealthChange(player, health, maxHealth, health, restoreType) 
       else
         removeEvent(nextHeal[RestoreType.item])
       end
@@ -81,10 +173,16 @@ function AutoHeal.ConnectCastListener(listener)
   end
 
   connect(LocalPlayer, { onHealthChange = AutoHeal.executeCast })
+  connect(g_game, { onTalk = AutoHeal.onTalk,
+					onSpellCooldown = AutoHeal.onSpellCooldown,
+					onSpellGroupCooldown = AutoHeal.onSpellGroupCooldown})
 end
 
 function AutoHeal.DisconnectCastListener(listener)
   disconnect(LocalPlayer, { onHealthChange = AutoHeal.executeCast })
+  disconnect(g_game, { onTalk = AutoHeal.onTalk,
+					onSpellCooldown = AutoHeal.onSpellCooldown,
+					onSpellGroupCooldown = AutoHeal.onSpellGroupCooldown})
 end
 
 function AutoHeal.executeItem(player, health, maxHealth, oldHealth)
@@ -99,8 +197,10 @@ function AutoHeal.ConnectItemListener(listener)
   end
 
   connect(LocalPlayer, { onHealthChange = AutoHeal.executeItem })
+  connect(g_game, { onMultiUseCooldown = AutoHeal.onMultiUseCooldown })
 end
 
 function AutoHeal.DisconnectItemListener(listener)
   disconnect(LocalPlayer, { onHealthChange = AutoHeal.executeItem })
+  disconnect(g_game, { onMultiUseCooldown = AutoHeal.onMultiUseCooldown })
 end
